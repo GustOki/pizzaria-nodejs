b282195303e6ed05200fdc59be270f36c0012002
Gustavo Oki <gustavooki.dev@gmail.com>
2023-04-17 20:25:43 -0300

    Feat: CRUD Employee Service

---
+++ src/service/funcionario.service.js
@@ -0,0 +1,38 @@
+const Funcionario = require("../model/Funcionario");
+
+const findEmployeeByIdService = (id) => {
+    return Funcionario.findById(id);
+};
+
+const findAllEmployeesService = () => {
+    return Funcionario.find();
+};
+
+const createEmployeeService = (body) => {
+    return Funcionario.create(body);
+};
+const updateEmployeeService = (id, body) => {
+    return Funcionario.findByIdAndUpdate(id, body { returnDocument: "after" });
+};
+
+const removeEmployeeService = (id) => {
+    return Funcionario.findByIdAndRemove(id);
+};
+
+const addEmployeeAddressService = (id, endereco) => {
+
+};
+
+const removeEmployeeAddressService = (id) => {
+
+}
+
+module.exports = {
+    findEmployeeByIdService,
+    findAllEmployeesService,
+    createEmployeeService,
+    updateEmployeeService,
+    removeEmployeeService,
+    addEmployeeAddressService,
+    removeEmployeeAddressService
+}
