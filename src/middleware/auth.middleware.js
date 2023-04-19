ce5f8a89e40be496378f971b61df94f3208427f6
Gustavo Oki <gustavooki.dev@gmail.com>
2023-04-18 21:05:25 -0300

    Feat: Autorizacao e middleware

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,38 @@
+const jwt = require("jsonwebtoken");
+const { findEmployeeByIdService } = require("../service/funcionario.service");
+
+module.exports = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader){
+        return res.status(401).send({ message: "O token nao foi informado!"});
+    };
+
+    const parts = authHeader.split(" "); //["Bearer, <token>"]
+
+    if(parts.length !== 2){
+        return res.status(401).send({ message: "Token invalido!"});
+    };
+
+    const [schema, token] = parts;
+
+    if(!/^Bearer$/i.test(schema)){
+        return res.status(401).send({ message: "Token malformado!"});
+    };
+
+    jwt.verify(token, "9ouojwjfwmsfkokdeoij28miaovwjijimi3fcir", async (err, decoded) => {
+        if(err){
+            return res.status(500).send({ message: "Token invalido!"});
+        };
+
+        const employee = await findEmployeeByIdService(decoded.id);
+
+        if(!employee || !employee.id){
+            return res.status(401).send({ message: "Token invalido!"});
+        };
+
+        req.employeeId = decoded.id;
+
+        return next();
+    })
+}
